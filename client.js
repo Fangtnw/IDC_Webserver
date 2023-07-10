56acefe7ffc13809d6e51c2b4f57e537fc688e1d
thanawat <thanawat.smkn@gmail.com>
2023-07-10 18:55:24 +0700

    edit host

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('http://192.168.0.103:3000'); 
+//const socket = io.connect('http://localhost:3000'); 
 const readline = require('readline');
+const rclnodejs = require('rclnodejs');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -32,19 +34,35 @@ rl.question('Enter the password: ', (password) => {
 //   [0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1],
 //   [1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0],
 // ];
+rclnodejs.init().then(() => {
+  const node = rclnodejs.createNode('publisher_node');
+  publisher = node.createPublisher('std_msgs/msg/String', '/server_status');
+});
 
-// // Publish object status to server
+  // // Publish object status to server
 // socket.emit('object_status', objectStatus);
 
 // Receive command from server
   socket.on('command', (data) => {
     console.log('Received command:', data);
-    if (data.player === myRole) {
-      // Handle the command for player1
+    if ((data.player === myRole || data.player === 'all') && data.command === 'start') {
+      if (publisher) {
+        publisher.publish('start');
+        console.log('Published message to ROS 2 topicss');
+      } else {
+        console.log('ROS 2 publisher is not initialized');
+      }
+      console.log('starting the controller')
+    } else if ((data.player === myRole || data.player === 'all') && data.command === 'spawn_object') {
+      // Call ros2 service spawn_entity
       // ...
-    } else if (data.player === myRole) {
-      // Handle the command for player2
-      // ...
+      // if (publisher) {
+      //   publisher.publish('spawn');
+      //   console.log('Published message to ROS 2 topic');
+      // } else {
+      //   console.log('ROS 2 publisher is not initialized');
+      // }
+      // console.log('spawning the object')
     }
   });
 
