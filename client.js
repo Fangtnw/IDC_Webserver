2c3a2db9c954a97a2af161be8f63a05b2e8585fb
thanawat <thanawat.smkn@gmail.com>
2023-07-21 13:16:43 +0700

    add spawn object

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
 const io = require('socket.io-client');
-//const socket = io.connect('http://192.168.194.230:3000'); 
+//const socket = io.connect('http://192.168.194.1:4000'); 
 const socket = io.connect('http://localhost:4000'); 
 const readline = require('readline');
 const rclnodejs = require('rclnodejs');
-const { spawn } = require('child_process');
 
 const yaml = require('yaml');
 const fs = require('fs');
@@ -15,21 +14,23 @@ const password = config.RoomId;
 const username = config.Team;
 const myRole = config.Role;
 
+
+
 // const env = require('dotenv').config()
 //  const password = process.env.RoomId
 //  const username = process.env.Team
 //  const myRole = process.env.Role
-  console.log('Waiting for the server . . .')
+console.log('Waiting for the server . . .')
 
-  socket.emit('join', { password });
+socket.emit('join', { password });
 
   // Receive authentication result from server
-  socket.on('authResult', (data) => {
-    if (data.success) {
-      console.log('Successfully joined the room.');
-      console.log('Welcome', username); console.log('you are', myRole);
-      if (myRole !== 'player1' && myRole !== 'player2') 
-        { console.log("\x1b[31mplease identify your role correctly\x1b[0m"); }
+socket.on('authResult', (data) => {
+  if (data.success) {
+    console.log('Successfully joined the room.');
+    console.log('Welcome', username); console.log('you are', myRole);
+    if (myRole !== 'player1' && myRole !== 'player2') 
+      { console.log("\x1b[31mplease identify your role correctly\x1b[0m"); }
 
     } else {
       console.log('Failed to join the room. Wrong password.');
@@ -37,36 +38,60 @@ const myRole = config.Role;
     }
   });
   
-  const loginReport = {
-    role : myRole,
-    team : username,}
+const loginReport = {
+  role : myRole,
+  team : username,}
 
-  socket.emit('login', loginReport);
+socket.emit('login', loginReport);
 
   // socket.on('playerRole', (data) => {
   //   myRole = data.player;
   //   console.log('you are', myRole);
   // });
 
-  rclnodejs.init().then(() => {
-  const node = new rclnodejs.Node("client_node");//rclnodejs.createNode('client_node');
-  publisher = node.createPublisher('std_msgs/msg/String', '/server_status');
-  const subscriber = node.createSubscription(
-    'std_msgs/msg/Int16MultiArray',
-    '/score_report',
-    (message) => {
-      console.log(`scoring report': ${message}`);
-      // const scoringReport = `Scoring report: ${username}, Value: ${message.data}`;
-      const scoringReport = {
-        role : myRole,
-        team : username,
-        score : message.data[0], // score data or mission status
-        status : message.data[1],
-      };
-      socket.emit('scoringReport', scoringReport);
-      socket.emit('login', loginReport);
+rclnodejs.init().then(() => {
+node = new rclnodejs.Node("client_node");//rclnodejs.createNode('client_node');
+publisher = node.createPublisher('std_msgs/msg/String', '/server_status');
+const spawn_client = node.createClient('msg_interfaces/srv/SpawnObj','/spawn_command');
+
+const subscriber = node.createSubscription(
+  'std_msgs/msg/Int16MultiArray',
+  '/score_report',
+  (message) => {
+    if (message.data[1] === 0 || message.data[1] === 1)
+    {console.log(`scoring report': score : ${message.data[0]}`);
+     socket.emit('scoringReport', scoringReport);
+    }
+    else if (message.data[1] === 3)
+    {console.log('Time is UPPP');}
+    // cont scoringReport = `Scoring report: ${username}, Value: ${message.data}`;
+    
+    const scoringReport = {
+      role : myRole,
+      team : username,
+      score : message.data[0], // score data or mission status
+      status : message.data[1],
+    }
+      
+    socket.emit('login', loginReport);
     }
   );
+
+socket.on('command', (data) => {
+  console.log('Received command:', data);
+  if ((data.player === myRole || data.player === 'all') && data.command === 'start') {
+      console.log('starting the controller')
+      //call start game service
+    } else if ((data.player === myRole || data.player === 'all') && data.command === 'spawn_object') {
+      console.log('spawning the object')
+      const request = {spawn_command: {}}
+      spawn_client.sendRequest(request, (response) => {
+        console.log(`Result: ${typeof response}`, response);
+      });
+      console.log('already sent object')
+
+    }
+  });
 
   rclnodejs.spin(node);
 });
@@ -78,27 +103,4 @@ process.on('SIGINT', () => {
   process.exit(0);
 });
 
-  // // Publish object status to server
-// socket.emit('object_status', objectStatus);
-
-// Receive command from server
-  socket.on('command', (data) => {
-    console.log('Received command:', data);
-    if ((data.player === myRole || data.player === 'all') && data.command === 'start') {
-      // if (publisher) {
-      //   publisher.publish('start');
-      //   console.log('Published message to ROS 2 topicss');
-      // } else {
-      //   console.log('ROS 2 publisher is not initialized');
-      // }
-      console.log('starting the controller')
-      
-      //call start game service
-    } else if ((data.player === myRole || data.player === 'all') && data.command === 'spawn_object') {
-      
-      console.log('spawning the object')
-      //call spawn object service
-    }
-    
-  });
-
+  
